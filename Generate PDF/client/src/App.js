86649f2d225f572a90217d77d9bd83f0ac87bde0
diana-mans <115227596+diana-mans@users.noreply.github.com>
2023-03-17 16:23:33 +0300

    styles added

--- Generate PDF/client/src/App.js
+++ Generate PDF/client/src/App.js
@@ -1,43 +1,64 @@
-import './App.css';
+import styles from './App.scss';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
 import React from 'react';
-
 
 class App extends React.Component {
   //Изначальное состояние приложения
   state = {
     name: '',
     receiptId: 0,
-    price1:  0,
+    price1: 0,
     price2: 0,
   };
   //Перехватывает то, что ввел пользователь и обновляет состояние приложения
-  handleChange = ({target: { value, name }}) => this.setState({ [name]: value });
+  handleChange = ({ target: { value, name } }) => this.setState({ [name]: value });
 
   //Создает и скачивает файл PDF
   createAndDownloadPdf = () => {
-    //Axios.post отправляет запрос на сервер 
+    //Axios.post отправляет запрос на сервер
     //Первый параметр - url
     //Второй параметр - тело запроса (обычно объекты)
-    axios.post('/create-pdf', this.state)
+    axios
+      .post('/create-pdf', this.state)
       //После выполнения запроса методом get выполяет еще один запрос на сервер
       .then(() => axios.get('fetch-pdf', { responseType: 'blob' }))
       .then((res) => {
         const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
 
         saveAs(pdfBlob, 'newPdf.pdf');
-      })
-  }
+      });
+  };
   //Рендерит наш state
   render() {
     return (
       <div className="App">
-        <input type="text" placeholder="Name" name="name" onChange={this.handleChange} />
-        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
-        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
-        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
-        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+        <input type="text" placeholder="Customer name" name="name" onChange={this.handleChange} />
+        <br />
+        <input
+          type="number"
+          placeholder="Receipt ID"
+          name="receiptId"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="number"
+          placeholder="First item price"
+          name="price1"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="number"
+          placeholder="Second item price"
+          name="price2"
+          onChange={this.handleChange}
+        />
+        <br />
+        <button className="btn btn-success" onClick={this.createAndDownloadPdf}>
+          Download PDF
+        </button>
       </div>
     );
   }
--- Generate PDF/server/index.js
+++ Generate PDF/server/index.js
@@ -13,24 +13,22 @@ const port = process.env.PORT || 5000;
 //Чтобы express.js использовал cors
 app.use(cors());
 //Чтобы express.js использовал body-parser и брал все запросы, кроме строк
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Отправляем запрос
 app.post('/create-pdf', (req, res) => {
-    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
-        if(err) {
-            res.send(Promise.reject());
-        }
-
-        res.send(Promise.resolve());
-    })
-})
+  pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+    if (err) {
+      res.send(Promise.reject());
+    }
+    res.send(Promise.resolve());
+  });
+});
 
 //Получаем запрос
 app.get('/fetch-pdf', (req, res) => {
-    res.sendFile(`${__dirname}/result.pdf`)
-})
+  res.sendFile(`${__dirname}/result.pdf`);
+});
 
 app.listen(port, () => console.log(`Listeninig om port ${port}`));
-
