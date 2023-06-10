462082f9e201d6790ab554af51f982826ac25fc6
Diana Mansurova <diana.mans2000@gmail.com>
2023-06-10 13:52:26 +0500

    1.4. lession. react and css-modules added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,10 +1,30 @@
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const typescriptsLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
-  return [typescriptsLoader];
+
+  const stylesLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]__[hash:base64:4]' : '[hash:base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
+
+  return [typescriptsLoader, stylesLoader];
 }
