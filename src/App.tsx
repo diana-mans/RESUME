b0123fad55dc7109fb2b7f2895e9ff2d701a76b8
Diana Mansurova <diana.mans2000@gmail.com>
2023-06-10 14:51:42 +0500

    1.6.lession. React-router and code splitting added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React, { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async';
+import MainPage from './components/pages/MainPage/MainPage';
 
 const App = () => {
   return (
-    <div>
-      <Counter />
+    <div className="app">
+      <Link to={'/'}>Главная</Link>
+      <br />
+      <Link to={'/about'}>О сайте</Link>
+      <br />
+      <br />
+      <Routes>
+        <Route path={'/'} element={<MainPage />} />
+        <Route
+          path={'/about'}
+          element={
+            <Suspense fallback={<h1>Загрузка</h1>}>
+              <AboutPageAsync />
+            </Suspense>
+          }
+        />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MainPage = () => {
+  return <div>Main Page</div>;
+};
+
+export default MainPage;
