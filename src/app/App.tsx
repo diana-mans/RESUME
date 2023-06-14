6b5819c4a2484383ce551c2ab10cd058f483e82d
Diana Mansurova <diana.mans2000@gmail.com>
2023-06-14 19:31:24 +0500

    12. Navbar and links separated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,14 @@
-import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { useTheme } from './providers/ThemeProvider';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
+import { Navbar } from '@/widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <br />
-      <Link to={'/about'}>О сайте</Link>
-      <br />
-      <br />
+      <Navbar />
       <AppRouter />
       <button onClick={toggleTheme}>Toggle theme</button>
     </div>
