70f75f8239ea847066b00077366509a231a6484b
Diana Mansurova <diana.mans2000@gmail.com>
2023-06-21 01:47:58 +0500

    21. Loader and NotFoundPage added

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,11 @@
 import { RouteConfig } from '@/shared/config/routeConfig/routeConfig';
+import { PageLoader } from '@/widgets/PageLoader';
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<h1>Загрузка</h1>}>
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(RouteConfig).map(({ element, path }) => (
           <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
---
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -0,0 +1,25 @@
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './Loader.module.scss';
+
+interface LoaderProps {
+  className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+  return (
+    <div className={classNames(cls.ldsSpinner, {}, [className])}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
